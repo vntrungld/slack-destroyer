7efcab1225be233a7f0f4f57073b606e2dc99087
trungld <vn.trungld@gmail.com>
2017-05-06 10:35:09 +0700

    Update: try catch for exec

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var exec = require('child_process').exec;
+var execSync = require('child_process').execSync;
 
 var config = JSON.parse(fs.readFileSync(`${process.env.HOME}/.slack-destroyer`));
 var tokens = config.tokens;
@@ -7,30 +7,58 @@ var channels = config.channels;
 var groups = config.groups;
 var directs = config.directs;
 var before = config.before;
-var commands = '';
+var command = '';
 
 tokens.forEach(function(token) {
+  console.log(`For the token: ${token}`)
   if (channels) {
+    console.log('Destroying channels:')
     channels.forEach(function(channel) {
-      commands += `slack-cleaner --token ${token} --message --channel ${channel} --user "*" --before=${before} --perform;`;
+      console.log(`Channel ${channel}...`);
+      command = `slack-cleaner --token ${token} --message --channel ${channel} --user "*" --before=${before} --perform;`;
+      command += `slack-cleaner --token ${token} --message --channel ${channel} --bot --perform;`;
+      try {
+        execSync(command);
+      } catch(err) {
+        console.log(err);
+      }
+      console.log(`Done for ${channel}`);
     });
+    console.log('Done for channels');
   }
 
   if (groups) {
+    console.log('Destroying groyps:');
     groups.forEach(function(group) {
-      commands += `slack-cleaner --token ${token} --message --group ${group} --user "*" --before=${before} --perform;`;
+      console.log(`Group ${group}`);
+      command = `slack-cleaner --token ${token} --message --group ${group} --user "*" --before=${before} --perform;`;
+      try {
+        execSync(command);
+      } catch(err) {
+        console.log(err);
+      }
+      console.log(`Done for ${group}`);
     });
+    console.log('Done for groups');
   }
 
   if (directs) {
-    commands += `slack-cleaner --token ${token} --message --mpdirect ${directs.toString()} --user "*" --before=${before} --perform;`;
+    console.log('Destroying directs');
+    command = `slack-cleaner --token ${token} --message --mpdirect ${directs.toString()} --user "*" --before=${before} --perform;`;
+    try {
+      execSync(command);
+    } catch(err) {
+      console.log(err);
+    }
+    console.log('Done for direct');
   }
 
-  commands += `slack-cleaner --token ${token} --file --user "*" --before=${before} --perform;`;
-});
-
-exec(commands, function(error, stdout, stderr) {
-  console.log(error);
-  console.log(stdout);
-  console.log(stderr);
+  console.log('Destroying files');
+  command = `slack-cleaner --token ${token} --file --user "*" --before=${before} --perform;`;
+  try {
+    execSync(command);
+  } catch(err) {
+    console.log(err);
+  }
+  console.log('Done for file');
 });
